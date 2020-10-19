024c2f4a1ea6931b905acff652600397846dec61
functionsOnly (Brian) <47933136+functionsonly@users.noreply.github.com>
2020-10-19 02:04:25 -0700

    Update gmapsDistanceTool-v0.1.1.js

--- gmapsDistanceTool-v0.1.1.js
+++ gmapsDistanceTool-v0.1.1.js
@@ -9,8 +9,9 @@
 
 	Install:
 		1.  Load gmapsDistanceTool.js after the required dependencies are loaded
-		2.  Create a div inside the Google Map container, eg:
-			<div id="map"><div id="distanceTool"></div></div>
+		2.  Create a div outside of the Google Map container, eg:
+			<div id="map"></div>
+			<div id="distanceTool"></div>
 		3.  Initiate distance tool after Google Maps object is available, 
 		    in this case 'map' is the Google Map object, ie:
 			var map = new google.maps.Map(document.getElementById('map'), {});
@@ -33,6 +34,7 @@
 	Updated:
 		2019-Feb-21 -- v0.1.0 -- Epoch
 		2019-Mar-03 -- v0.1.1 -- Updated for Boostrap 4 only 
+		2020-Oct-19 -- v0.1.2 -- Updated div container instructions
 */
 
 /* ===============================================================================
